c2e2e432dc132eaa6e34b45d01caf884af45b0c1
Ofer Tauber <ofertauber@gmail.com>
2022-05-13 08:35:30 +0300

    Basic ChartIt

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,19 @@
 import { getData, organizeData } from './get_data.js';
+import { chsrtIt } from './chsrt_it.js';
 
 const reset = async function () {
   try {
-    const data = await getData();
-    const temp = await organizeData(data.covidData.data, data.regionsData);
-    console.log(temp);
+    const rawData = await getData();
+    const dataBaseline = await organizeData(
+      rawData.covidData.data,
+      rawData.regionsData
+    );
+    const initialChartData = proccecDataToChart(
+      dataBaseline.asia,
+      'Confirmed cases in Asia',
+      'confirmed'
+    );
+    const myChart = await chsrtIt(chartContext, initialChartData);
   } catch (err) {
     errorMessage(err);
   }
@@ -15,43 +24,14 @@ const errorMessage = (err) => {
   console.log(err);
 };
 
+const chartContext = document.querySelector('#my-chart').getContext('2d');
+
 reset();
-
-// const myChart1 = document.querySelector('#my-chart').getContext('2d');
-
-// const myChart = new Chart(myChart1, {
-//   type: 'bar',
-//   data: {
-//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//     datasets: [
-//       {
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.5)',
-//           'rgba(54, 162, 235, 0.2)',
-//           'rgba(255, 206, 86, 0.2)',
-//           'rgba(75, 192, 192, 0.2)',
-//           'rgba(153, 102, 255, 0.2)',
-//           'rgba(255, 159, 64, 0.2)',
-//         ],
-//         borderColor: [
-//           'rgba(255, 99, 132, 1)',
-//           'rgba(54, 162, 235, 1)',
-//           'rgba(255, 206, 86, 1)',
-//           'rgba(75, 192, 192, 1)',
-//           'rgba(153, 102, 255, 1)',
-//           'rgba(255, 159, 64, 1)',
-//         ],
-//         borderWidth: 1,
-//       },
-//     ],
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true,
-//       },
-//     },
-//   },
-// });
+const proccecDataToChart = (data, label, key) => {
+  console.log(data);
+  return {
+    chartLabel: label,
+    valuesX: data.map((cuntry) => cuntry.name),
+    valuesY: data.map((cuntry) => cuntry[key]),
+  };
+};
